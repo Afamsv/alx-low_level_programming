30f9064680f0a14b81460bfb56833fc9cad91733
Afamsv <afamv76@gmail.com>
2022-09-11 14:41:04 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,26 +11,12 @@
 
 int main(void)
 {
-	int d, p;
+	char letter;
 
-	for (d = '0'; d < '9'; d++)
-	{
-		for (p = d + 1; p <= '9'; p++)
-		{
-			if (p != d)
-			{
-				putchar(n);
-				putchar(d);
-				putchar(p);
-				
-				if (d == '8' && p == '9')
-					continue;
-				
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
-	putchar('\n');
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n'):
+
 	return (0);
 }
