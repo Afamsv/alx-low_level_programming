6e6c540e21317303b39dc53189d2d623dd1fdb6c
Afamsv <afamv76@gmail.com>
2022-09-21 15:48:14 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,24 +1,16 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0 for success
+* main - Test function for positive or negative
+* Return: 0
 */
 
 int main(void)
 {
 	int i;
 
-	printf("Infinite loop incoming :(\n");
-	
 	i = 0;
-
-	/*while (i < 10)*/
-	/*{*/
-	/*putchar(i);*/
-	/*}*/
-
-	printf("Infinite loop avoided! \\o/\n");
+	positive_or_negative(i);
 
 	return (0);
 }
