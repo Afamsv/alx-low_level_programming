a1ef7b7567c3fc115f8cb291bdb1af9598c28a17
Afamsv <afamv76@gmail.com>
2022-10-01 06:57:17 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 #define UNUSED(x) (void)(x)
 
 /**
 * main - main function
 * @argc: argumentc
-* @argv: vector of arguments 
+* @argv: vector of arguments
 * Return: always 0
 */
 
@@ -15,4 +15,3 @@ int main(int argc, char *argv[])
 	printf("%s\n", argv[0]);
 	return (0);
 }
-
