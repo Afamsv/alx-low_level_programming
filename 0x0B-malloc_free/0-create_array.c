935b15b706d537fa103ebf0b7711df9c9ea858ad
Afamsv <afamv76@gmail.com>
2022-10-03 22:45:59 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,16 +10,19 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *str;
+	char *cr;
 	unsigned int i;
 
-	str = malloc(sizeof(char) * size);
+	if (size == 0)
+		return (NULL);
 
-	if (size == 0 || str == NULL)
-		return (NUll);
+	cr = malloc(sizeof(c) * size);
+
+	if (cr == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
-		strP[i] = c;
+		cr[i] = c;
 
-	return (str);
+	return (cr);
 }
