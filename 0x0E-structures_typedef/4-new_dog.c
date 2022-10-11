2895944811b26bc21f68ce7ff2a86d7f65a1b56b
Afamsv <afamv76@gmail.com>
2022-10-11 05:07:10 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,19 +1,19 @@
 #include "dog.h"
 #include <stdlib.h>
 
-
 int _strlen(char *str);
 char *_strcopy(char *dest, char *src);
 dog_t *new_dog(char *name, float age, char *owner);
 
-/** _strlen - Finds the length of a string.
+/**
+* _strlen - Finds the length of a string.
 * @str: The string to be measured.
 * Return: The length of the string.
 */
 
 int _strlen(char *str)
 {
-	int len = 0;
+	int lent = 0;
 
 	while (*str++)
 		len++;
@@ -31,7 +31,7 @@ int _strlen(char *str)
 
 char *_strcopy(char *dest, char *src)
 {
-	int index = 0;
+	int dex = 0;
 
 	for (index = 0; src[index]; index++)
 		dest[index] = src[index];
@@ -64,7 +64,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (doggo->name == NULL)
 	{
 		free(doggo);
-		return (NULL);
+		return(NULL);
 	}
 
 	doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
@@ -72,7 +72,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		free(doggo->name);
 		free(doggo);
-		ereturn (NULL);
+		return (NULL);
 	}
 
 	doggo->name = _strcopy(doggo->name, name);
