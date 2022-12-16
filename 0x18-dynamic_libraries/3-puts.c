f821ed7cf6fa4d61f455cc28b9b6c9dca8442ad2
Afamsv <afamv76@gmail.com>
2022-12-16 15:03:08 -0800

    Dynamic libraries

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _puts - function that print a string, followed by new line
+* @str: pointer that point the string
+* Return: nothing
+*/
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
